7d4f344cf5d5298487af6540b481306ba4dbe958
Fernando Melo <65089035+Nandosts@users.noreply.github.com>
2023-09-03 12:01:30 -0300

    feat: add loading screen

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,17 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+
+window.addEventListener('load', function () {
+  var loadingSpinner = document.getElementById('loading-spinner');
+  if (loadingSpinner) {
+    // Set a short delay to ensure a smooth fade-out effect
+    setTimeout(function () {
+      loadingSpinner.style.opacity = 0;
+      // After the opacity transition, hide the spinner
+      setTimeout(function () {
+        loadingSpinner.style.display = 'none';
+      }, 300);
+    }, 300);
+  }
+});
