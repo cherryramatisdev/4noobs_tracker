fcb4dfa4ce077e4780cf16d195efc30f2cc1822d
William Rodrigues <williamrodrigues2442@gmail.com>
2023-09-15 18:25:11 -0300

    tests: fix tests

--- spec/models/repository_spec.rb
+++ spec/models/repository_spec.rb
@@ -3,21 +3,21 @@ require 'rails_helper'
 RSpec.describe Repository, type: :model do
   context 'when saving repository' do
     it 'should save with valid name' do
-      repository = Repository.new(name: 'test', owner: 'test', url: 'http://test.com', technology: 'A')
+      repository = Repository.new(name: 'test', owner: 'test', url: 'http://test.com', technology: 'A', technology_image_pattern: 'a')
       expect(repository.valid?).to be_truthy
       expect(repository.save).to be_truthy
     end
 
     it 'should not save without name' do
-      repository = Repository.new(owner: 'test', url: 'http://test.com', technology: 'A')
+      repository = Repository.new(owner: 'test', url: 'http://test.com', technology: 'A', technology_image_pattern: 'a')
       expect(repository.valid?).to be_falsy
       expect(repository.save).to be_falsy
       expect(repository.errors[:name]).to include(%{can't be blank})
     end
 
     it 'should not save duplicate name' do
-      Repository.create(name: 'test', owner: 'test', url: 'http://test.com', technology: 'A')
-      repository = Repository.new(name: 'test', owner: 'test', url: 'http://test.com', technology: 'A')
+      Repository.create(name: 'test', owner: 'test', url: 'http://test.com', technology: 'A', technology_image_pattern: 'a')
+      repository = Repository.new(name: 'test', owner: 'test', url: 'http://test.com', technology: 'A', technology_image_pattern: 'a')
       expect(repository.valid?).to be_falsy
       expect(repository.save).to be_falsy
       expect(repository.errors[:name]).to include(%{has already been taken})
